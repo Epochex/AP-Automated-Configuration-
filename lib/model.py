a7eaef6e2273e00d1abb15826ce611046f7137c8
AaronLin <68621077+Epochex@users.noreply.github.com>
2024-07-21 18:46:40 +0200

    final stable version

--- lib/model.py
+++ lib/model.py
@@ -1,130 +1,78 @@
-from PyQt5.QtCore import QThread, pyqtSignal
-import time
-import os
-import re
-import pandas as pd
+import os,re,pandas as pd
 from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 
-LANG = []
-ONLINE = []
 
-# def get_net_segment():
-#     with os.popen("arp -a") as res:  # 执行 arp -a ，返回一个可读取的文件对象 res
-#         count = 0
-#         for line in res:
-#             line = line.strip()  # 移除行首和行尾的空白字符。
-#             if line.startswith("接口") or line.startswith("Interface"): # 检查行是否以“接口”或“Interface”开头。
-#                 count = count + 1  # 计数器加1，用于检测第几次出现网络接口行
-#                 net_segment = re.findall(
-#                     "(\d+\.\d+\.\d+)\.\d+", line)[0]  # 使用正则表达式提取网络段（三段IP地址）
-#             if count == 2:   # 在找到第二个网络接口行时结束
-#                 return 0     
-#     return net_segment   # 返回网段
-
-#原先的get_net_segment改为get_network_interfaces和get_net_segments
-
-
-# def clear_arp_cache():
-#     result = os.system('arp -d')
-#     if result == 0:
-#         print("ARP cache has been cleared.")
-#     else:
-#         print("Failed to clear ARP cache.")
-# 用 netsh 命令来清除 ARP 缓存,有出现过好几次arp参数错误
-
-def clear_arp_cache():
-    result = os.system('netsh interface ip delete arpcache')
-    if result == 0:
-        print("ARP cache has been cleared.")
-    else:
-        print("Failed to clear ARP cache. \n Check that you are using a PowerShell(administrator) to start the script")
-
-
-
-def get_network_interfaces():
-    interfaces = []
+def get_network_ips():
+    ips = []
     # 获取所有网络接口的名称（别名）
-    command = 'powershell "Get-NetIPAddress | Select-Object -ExpandProperty InterfaceAlias"'
+    command = 'ipconfig'
     
     # 使用 os.popen 执行 PowerShell 命令，并获取命令输出的结果
     with os.popen(command) as res:
         for line in res:
             line = line.strip()  # 去除每行的首尾空白字符
-            if line and line not in interfaces:  # 确保没有重复的接口名称
-                interfaces.append(line)  # 将接口名称添加到列表中
-    return interfaces  # 返回包含所有网络接口名称的列表
-
-def get_net_segments(interface):
-    net_segments = []
-    # 获取指定网络接口的所有 IPv4 地址，$_获取上一个管道符运过来的所有对象，AddressFamily= ipv4/ipv6 , ExpandProperty 展开说说叭 
-    # -InterfaceAlias 参数指定接口别名
-    # Where-Object 过滤条件，只获取 IPv4 地址
-    # Select-Object 提取 IPAddress 属性的值
-    command = f'powershell "Get-NetIPAddress -InterfaceAlias \'{interface}\' | Where-Object {{ $_.AddressFamily -eq \'IPv4\' }} | Select-Object -ExpandProperty IPAddress"'
-    
-    # 使用 os.popen 执行 PowerShell 命令，并获取命令输出的结果
-    with os.popen(command) as res:
-        for line in res:
-            line = line.strip()  # 去除每行的首尾空白字符
-            # 使用正则表达式匹配 IP 地址前三段
-            net_segment = re.findall(r"(\d+\.\d+\.\d+)\.\d+", line)
-            if net_segment:
-                net_segments.append(net_segment[0])  # 将匹配到的前三段 IP 地址添加到列表中
-    return net_segments  # 返回包含所有匹配到的网络段的列表
+            if line and line not in ips and line.startswith('IPv4 Address'):  # 确保没有重复的接口名称
+                ips.append(line.split(": ")[1])  # 将接口名称添加到列表中
+            elif line and line not in ips and line.startswith('IPv4 地址'):  # 确保没有重复的接口名称
+                ips.append(line.split(": ")[1])  # 将接口名称添加到列表中
+    return ips  # 返回包含所有网络接口名称的列表
 
 
-def ping_net_segments_all(net_segments): 
-    with ThreadPoolExecutor(max_workers=4) as executor:  # 最多可以有4个并发的线程同时运行
-        for net_segment in net_segments:
-            for i in range(1, 255):
-                executor.submit(os.popen, f"ping -w 1 -n 1 {net_segment}.{i}")  # -w 1，等待一秒，-n 1发起一个请求
+def ping_net_segment_all(net_segment):
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        for i in range(1, 255):
+            executor.submit(os.popen, f"ping -w 1 -n 1 {net_segment}.{i}")
 
-# def get_arp_ip_mac():
-#     header = None
-#     with os.popen("arp -a") as res:
-#         for line in res:
-#             line = line.strip()
-#             if not line or line.startswith("接口") or line.startswith("Interface"):
-#                 global LANG
-#                 if line.startswith("接口"):
-#                     LANG = ['接口', '物理地址', '类型', 'Internet 地址']
-#                 elif line.startswith("Interface"):
-#                     LANG = ['Interface', 'Physical Address', 'Type', 'Internet Address']
-#                 continue
-#             if header is None:
-#                 header = re.split(" {2,}", line.strip())
-#                 break
-#         # 读取 ARP 表数据
-#         df = pd.read_csv(res, sep=" {2,}", names=header, header=None, engine='python')
-    
-#     # 过滤以 98- 开头的 MAC 地址
-#     df_bool = df[LANG[1]].str.startswith("b0-")
-#     df = df[df_bool].reset_index(drop=True)
-    
-#     return df
 
-def get_arp_ip_mac():
-    global LANG
-    LANG = []
+def get_arp_ip_mac(ip):
     header = None
+    interface_found = False
     with os.popen("arp -a") as res:
+        
+        # get header
+        # res = res.readlines()
+        # print(res)
+        data = []
+
         for line in res:
             line = line.strip()
+            # print(line)
             if not line or line.startswith("接口") or line.startswith("Interface"):
+                global LANG
                 if line.startswith("接口"):
-                    LANG = ['接口', '物理地址', '类型', 'Internet 地址']
+                    LANG = ['接口','物理地址','动态', 'Internet 地址']
                 elif line.startswith("Interface"):
-                    LANG = ['Interface', 'Physical Address', 'Type', 'Internet Address']
+                    LANG = ['Interface','Physical Address','dynamic', 'Internet Address']
+                if ip in line:
+                    interface_found = True
+                else:
+                    interface_found = False
                 continue
             if header is None:
+                print(line)
                 header = re.split(" {2,}", line.strip())
-                break
-        # Read ARP table data
-        df_pretraite = pd.read_csv(res, sep=" {2,}", names=header, header=None, engine='python')
-    
-    # Filter MAC addresses starting with b0-
-    df = df_pretraite[df_pretraite[LANG[1]].str.startswith("98-")].reset_index(drop=True)
-    print(f"first filtred: {df}")
+                
+                continue
+            if interface_found:
+                if not line or line.startswith("Internet Address") or line.startswith("Internet 地址"):
+                    if header is None:
+                        header = re.split(" {2,}", line.strip())
+                    continue
+                # 收集ARP表中的数据
+                if line:
+                    data.append(re.split(" {2,}", line.strip()))
+        # choose AP who start with 98-
+        # df = pd.read_csv(res, sep=" {2,}",
+        #                  names=header, header=0, engine='python')
+        
+        if not data:
+            print(f"No data found for Interface: {ip}")
+            return pd.DataFrame()  # Return empty DataFrame if no data found
+        print(header)
+        df = pd.DataFrame(data, columns=header)
+        print(df)
+        df_bool = df[LANG[1]].str.startswith("98-")
+        df = df[df_bool].reset_index(drop=True)
     return df
 
 
@@ -136,50 +84,22 @@ def ping_ip_list(ips, max_workers=4):
             future_tasks.append(executor.submit(os.popen, f"ping -w 1 -n 1 {ip}"))
         wait(future_tasks, return_when=ALL_COMPLETED)
 
-def get_ap(interface):
-    clear_arp_cache()
 
-    net_segments = get_net_segments(interface)
-    if not net_segments:
-        return 0
-
-    ping_net_segments_all(net_segments)
-    df = get_arp_ip_mac()
+def get_ap(ip) -> pd.DataFrame:
+    # seg = get_net_segment()
+    seg_ip = re.findall(
+                    "(\d+\.\d+\.\d+)\.\d+", ip)[0]
+    ping_net_segment_all(seg_ip)
+    df = get_arp_ip_mac(ip)
     if LANG[0] == 'Interface':
         df = df.loc[df.Type == LANG[2], [LANG[3], LANG[1]]]
     elif LANG[0] == '接口':
         df = df.loc[df.类型 == LANG[2], [LANG[3], LANG[1]]]
-
     print("AP currently online:")
     if df.empty:
         print("No AP online, you can connect APs")
     else:
         print(df)
-
     ping_ip_list(df[LANG[3]].values)
+    print(ip)
     return df
-
-def get_ap_pass(last):
-    while 1:
-        df = get_arp_ip_mac()
-        if df.empty:
-            continue
-        if LANG[0] == 'Interface':
-            df = df.loc[df.Type == LANG[2], [LANG[3], LANG[1]]]
-            online = df.loc[~df['Physical Address'].isin(last['Physical Address'])]
-        elif LANG[0] == '接口':
-            df = df.loc[df.类型 == "动态", ["Internet 地址", "物理地址"]]
-            online = df.loc[~df.物理地址.isin(last.物理地址)]
-        if online.shape[0] > 0:
-            print("New AP:")
-            print(online)
-            i = 1
-            for k,v in online.values.tolist():
-                data = str(i) + '                    '+ k + '                    ' + v
-                i = i + 1
-            global ONLINE
-            ONLINE.append(data) if data not in ONLINE else ONLINE
-        time.sleep(10)
-        print(ONLINE)
-        ping_ip_list(df["Internet 地址"].values)
-        online = online.values.tolist()
--- lib/utils.py
+++ lib/utils.py
@@ -91,36 +91,33 @@ def range_of_ip(ip, submask):
 
 def get_config():
     path = "config" #文件夹目录
-    files= os.listdir(path) #得到文件夹下的所有文件名称
-    # for file in files: #遍历文件夹
-    #      if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开
-    #           f = open(path+"/"+file); #打开文件
-    #           iter_f = iter(f); #创建迭代器
-    #           str = ""
-    #           for line in iter_f: #遍历文件，一行行遍历，读取文本
-    #               str = str + line
-    #           s.append(str) #每个文件的文本存到list中
+    try:
+        files= os.listdir(path) #得到文件夹下的所有文件名称
+    except FileNotFoundError as e:
+        print(e)
+        os.mkdir(path)
+        files = []
     return files
 
 def get_file_config(file):
-    data = []  # 初始化一个空列表用于存储处理后的数据
-    with open(file, 'r', encoding='utf-8') as f:  # 打开文件，以读取模式和utf-8编码
-        lines_unhandled = f.readlines()  # 读取文件的所有行，并返回一个列表，每行作为列表中的一个元素
-        lines_unhandled = [x.strip() for x in lines_unhandled]  # 去除每行的前后空白字符
-        lines = list(set(lines_unhandled))  # 去除重复行
-        lines.sort()  # 对行进行排序
-        lines.remove('')  # 移除空行
-        print(lines)  # 输出处理后的行，供调试使用
-        if lines:  # 如果处理后的行列表不为空
-            for line in lines:  # 遍历每一行
-                line = line.strip()  # 去除每行的前后空白字符
-                # 忽略以 "#" 开头的行，或者包含 "=;"、"= ;"、"=  ;" 的行
+    data = []
+    with open(file,'r',encoding='utf-8') as f:
+        lines_unhandled = f.readlines()
+        lines_unhandled = [x.strip() for x in lines_unhandled]
+        lines = list(set(lines_unhandled))
+        lines.sort()
+        try:
+            lines.remove('')
+        except Exception as e:
+            print(e)
+        print(lines)
+        if lines:
+            for line in lines:
+                line = line.strip()
                 if line[0] == "#" or "=;" in line or "= ;" in line or "=  ;" in line:
                     continue
-                line = line.split(';')[0]  # 按照分号分割行，并取第一部分
-                if "descript" not in line:  # 如果行中不包含 "descript" 字符串
-                    line = ''.join(line.split())  # 移除行中的所有空白字符
-                # 对单引号进行转义处理，将单引号替换为反斜杠加单引号
-                line = line.replace("'", "\\'")
-                data.append(line)  # 将处理后的行添加到数据列表中
-    return data  # 返回处理后的数据列表
+                line = line.split(';')[0]
+                if "descript" not in line:
+                    line = ''.join(line.split())
+                data.append(line)
+    return data
